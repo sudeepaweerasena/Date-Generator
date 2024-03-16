b7b526f280ebffce704578113c019f63ae0409c8
sudeepaweerasena <106436944+sudeepaweerasena@users.noreply.github.com>
2024-03-16 11:12:00 +0530

    Date-Generator
    Date-Generator

---
+++ javascri.js
@@ -0,0 +1,26 @@
+let day;
+switch(new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wendsday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+}
+
+document.getElementById("today").innerHTML = "Today is " + day;
